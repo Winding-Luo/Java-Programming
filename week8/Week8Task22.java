8d378975b627223561184e6e71d42c7f38eb2281
Winding-Luo <3087615590@qq.com>
2024-10-28 11:19:42 +0800

    Update Week8Task22.java

--- week8/Week8Task22.java
+++ week8/Week8Task22.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class Week8Task22 {
     public static void main(String[] args) {
-        double [] nums = new double[10];
-        Scanner sc = new Scanner(System.in);
+        double [] a = new double[10];
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter 10 numbers:");
         for (int i = 0; i < 10; i++) {
-            nums[i] = sc.nextDouble();
+            a[i] = input.nextDouble();
         }
-        double minv = min(nums);
+        double minv = min(a);
         System.out.println("The minimun number is: " + minv);
     }
-    public static double min(double[] nums) {
-        double min = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < min) {
-                min = nums[i];
+    public static double min(double[] a) {
+        double min = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
             }
         }
         return min;
