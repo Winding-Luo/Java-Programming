0e62bd45529ad677ce58cce3404d896db4047b8d
Winding-Luo <3087615590@qq.com>
2024-10-28 11:18:27 +0800

    Update Week8Task23.java

--- week8/Week8Task23.java
+++ week8/Week8Task23.java
@@ -2,23 +2,29 @@ import java.util.Scanner;
 
 public class Week8Task23 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double []nums = new double[10];
+        
+        Scanner input = new Scanner(System.in);
+        double []a = new double[10];
+        
         System.out.println("Enter 10 numbers:");
         for (int i = 0; i < 10; i++) {
-            nums[i] = sc.nextDouble();
+            a[i] = input.nextDouble();
         }
-        double []temp = new double[10];
+
+        
+        double []b = new double[10];
         System.out.println("This is the primary numbers:");
         for (int i = 0; i < 10; i++) {
-            System.out.print(nums[i]);
+            System.out.print(a[i]);
             System.out.print(" ");
-            temp[10 - i - 1] = nums[i];
+            b[10 - i - 1] = a[i];
         }
+
+        
         System.out.println();
         System.out.println("This is the reverse numbers:");
         for (int i = 0; i < 10; i++) {
-            System.out.print(temp[i]);
+            System.out.print(b[i]);
             System.out.print(" ");
 
         }
