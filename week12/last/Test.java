b35ec96d1b44863fc14b1f7765711b57c2b69ea8
Winding-Luo <3087615590@qq.com>
2024-11-25 11:10:15 +0800

    Add files via upload

---
+++ week12/last/Test.java
@@ -0,0 +1,12 @@
+package last;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        Student student2 = new Student();
+
+        // Print the name
+        System.out.println("The name of student1 is " + student1.getName());
+        System.out.println("The name of student2 is " + student2.getName());
+    }
+}
